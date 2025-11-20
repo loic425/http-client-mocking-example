537d3204849cc43ef2fef3e1aa1a58e591b2e41b
Loïc Frémont <lc.fremont@gmail.com>
2025-11-20 16:09:45 +0100

    Add support for psr18

--- src/Command/BookClientCommand.php
+++ src/Command/BookClientCommand.php
@@ -2,13 +2,17 @@
 
 namespace App\Command;
 
+use Nyholm\Psr7\Request;
+use Psr\Http\Client\ClientInterface;
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Dumper;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(
@@ -18,26 +22,35 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 final class BookClientCommand extends Command
 {
     public function __construct(
-        private readonly HttpClientInterface $bookClient
+        private readonly HttpClientInterface $bookClient,
+        #[Autowire(service: 'psr18.book.client')]
+        private readonly ClientInterface $psr18BookClient,
     )
     {
         parent::__construct();
     }
 
-    protected function configure(): void
+    public function __invoke(
+        InputInterface $input,
+        OutputInterface $output,
+        #[Argument(suggestedValues: ['/new', '/books/9781484206485'])] ?string $path = null,
+        #[Option] ?bool $psr18 = null,
+    ): int
     {
-        $this
-            ->addArgument(name: 'path', mode: InputArgument::OPTIONAL, description: 'Path', default: '/new')
-        ;
-    }
+        $path ??= '/new';
+        $psr18 ??= false;
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
         $io = new SymfonyStyle($input, $output);
         $dump = new Dumper($output);
 
-        $response = $this->bookClient->request('GET', '/1.0'.$input->getArgument('path'));
+        if ($psr18) {
+            $response = $this->psr18BookClient->sendRequest(new Request('GET', '/1.0'.$path));
+            $io->write($dump($response->getBody()->getContents()));
 
+            return Command::SUCCESS;
+        }
+
+        $response = $this->bookClient->request('GET', '/1.0'.$path);
         $io->write($dump($response->toArray()));
 
         return Command::SUCCESS;
